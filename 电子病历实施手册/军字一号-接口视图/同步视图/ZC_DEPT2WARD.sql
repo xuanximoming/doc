a50c5ca11e7a9c4929ac5d4281df39e326d96c63
xuanximoming <yeshentianyue@sina.com>
2018-06-24 16:00:27 +0800

    delete

--- 电子病历实施手册/军字一号-接口视图/同步视图/ZC_DEPT2WARD.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW ZC_DEPT2WARD AS
-(
-SELECT distinct
-dept_vs_ward.ward_code deptid /*科室代码*/,
-dept_vs_ward.dept_code wardid /*病区代码*/,
-0 totalbed /*床位数*/
-FROM dept_vs_ward
-);
--- 电子病历实施手册/军字一号-接口视图/同步视图/zc_transfer.sql
+++
@@ -1,4 +0,0 @@
-create or replace view zc_transfer as
-select patient_id, visit_id, min(admission_date_time) adt_ward_datetime,max(discharge_date_time) dis_ward_datetime
-  from transfer
- group by patient_id, visit_id;
